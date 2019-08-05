ce8c32d113d23e3af9ba6a628b879d115c6861ed
Jason Shockness <jshock12@gmail.com>
2019-08-05 15:08:30 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -3,12 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
   end
   
-  def self.all 
-    @all
-  end
 
 end
 
